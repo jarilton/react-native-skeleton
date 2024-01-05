96353fe37af1b3a5fba5d7f258830d9f98e9ce6a
jariltonpereira <jarilton.pereira@grupofocus.com.br>
2024-01-04 23:02:29 -0300

    chore: skeleton loading

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, Text, View, useWindowDimensions } from "react-native";
+import { styles } from "./styles";
+
+import ContentLoader, { Circle, Rect } from "react-content-loader/native";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  const { width, height } = useWindowDimensions();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+
+      {loading ? (
+        <ContentLoader
+          viewBox={`0 0 ${width} ${height}`}
+          backgroundColor="#333"
+          foregroundColor="#999"
+        >
+          <Circle cx="38" cy="38" r="36" />
+          <Rect x="80" y="17" rx="4" ry="4" width={30} height={12} />
+          <Rect x="80" y="40" rx="4" ry="4" width={200} height={14} />
+        </ContentLoader>
+      ) : (
+        <View style={styles.header}>
+          <Image
+            source={{ uri: "https://github.com/jarilton.png" }}
+            style={styles.avatar}
+          />
+
+          <View>
+            <Text style={styles.greeting}>Olá</Text>
+            <Text style={styles.username}>Jamal</Text>
+          </View>
+        </View>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
